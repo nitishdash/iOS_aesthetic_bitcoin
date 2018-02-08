0263c042efe57c7e42a8d7519f815a53822c12de
Nitish Dash <nitishdash@Nitishs-MacBook-Pro.local>
2018-02-09 00:45:11 +0530

    Initial Commit

---
+++ Aesthetic Bitcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aesthetic Bitcoin
+//
+//  Created by Nitish Dash on 09/02/18.
+//  Copyright © 2018 Nitish Dash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
